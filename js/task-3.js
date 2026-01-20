26f4c1b1851dc5f700f63ce099fa6d0153d002e9
Onitrix <andriyproshak17@gmail.com>
2026-01-20 19:40:18 +0100

    do js-hw3

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function filterArray(numbers, value) {
+    let arr = [];
+
+    for (let newArray of numbers) {
+        if (newArray > value) {
+            arr.push(newArray);
+        }
+    }
+    return arr;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
